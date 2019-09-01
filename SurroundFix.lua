5cd8ae3e15819f00c0aa113347708d0c43a4afbe
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-01 04:24:48 +0100

    Fix for being in combat

--- SurroundFix.lua
+++ SurroundFix.lua
@@ -2,30 +2,38 @@ local AddonName = ...
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "ADDON_LOADED" then
         if AddonName ~= ... then
             return
         else
-            local altering
-            hooksecurefunc(UIParent, "SetPoint", function(self)
-                if altering then
-                    return
-                end
-
-                altering = true
-                self:SetSize(1920, 1080)
-                self:ClearAllPoints()
-                self:SetPoint("CENTER")
-                altering = nil
-            end)
-
-            UIParent:SetSize(1920, 1080)
-            UIParent:ClearAllPoints()
-            UIParent:SetPoint("CENTER")
-
             self:UnregisterEvent("ADDON_LOADED")
         end
     end
+
+    local altering
+    hooksecurefunc(UIParent, "SetPoint", function(self)
+        if InCombatLockdown() then
+            return
+        end
+        if altering then
+            return
+        end
+
+        altering = true
+        self:SetSize(1920, 1080)
+        self:ClearAllPoints()
+        self:SetPoint("CENTER")
+        altering = nil
+    end)
+
+    if InCombatLockdown() then
+        return
+    end
+
+    self:SetSize(1920, 1080)
+    self:ClearAllPoints()
+    self:SetPoint("CENTER")
 end)
