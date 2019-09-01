983fce1ca34adff1f7307f4dd00d316964005358
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-01 04:54:18 +0100

    Total rewrite to actually do things properly and prevent a million hooks

--- SurroundFix.lua
+++ SurroundFix.lua
@@ -1,116 +1,105 @@
+--------------------------------------------------------------------------------
+--Variables
+--------------------------------------------------------------------------------
 local sfixFrame = CreateFrame("Frame", "SurroundFixFrame")
+local rateLimit = 0.1 --Min time between the script being invoked from an event call
+local fixY --Get the Vertical resolution of the setup
+local fixX --Get the Horizontal resolution of the setup
+local fixYdiv
+local aspect = "unknown"
+local coreSet
+local parentDefault = true
 
-sfixFrame:RegisterEvent("ADDON_LOADED")
-
-function eventRegister() --Function to register events
-	sfixFrame:RegisterEvent("PLAYER_LOGIN")
-	sfixFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-	sfixFrame:RegisterEvent("DISPLAY_SIZE_CHANGED")
-end
-
-function eventUnregister() --Function to Unreguster those same events
-	sfixFrame:UnregisterEvent("PLAYER_LOGIN")
-	sfixFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	sfixFrame:UnregisterEvent("DISPLAY_SIZE_CHANGED")
-end
-
-function diagAnnounce()
-	print("UIParent Effective Scale -",UIParent:GetEffectiveScale())
-	print("Base -",GetScreenWidth(),"x",GetScreenHeight())
-	print("Floor Base -",floor(GetScreenWidth()+0.5),"x",floor(GetScreenHeight()+0.5))
-	print("Base Effective Res -",floor((GetScreenWidth()* UIParent:GetEffectiveScale())+0.5),"x",floor((GetScreenHeight()* UIParent:GetEffectiveScale())+0.5))
-end
-
-eventRegister() --Runs this to register once
 
 
 --------------------------------------------------------------------------------
+--Functions
+--------------------------------------------------------------------------------
+local function uiResolution()
+	fixY = GetScreenHeight() --Get the Vertical resolution of the setup
+	fixX = GetScreenWidth() --Get the Horizontal resolution of the setup
+	fixYdiv = fixY/9
 
-sfixFrame:SetScript("OnEvent", function(self, event, ...) --This is essentially saying "On an event, run this function of everything below"
-	local rateLimit = 0.5 --Min time between the script being invoked from an event call
-
-	local fixY = GetScreenHeight() --Get the Vertical resolution of the setup
-	local fixX  = GetScreenWidth()--Get the Horizontal resolution of the setup
-	local fixYdiv
-	local aspect = "unknown"
-
-
-	function uiResolution()
-		fixY = GetScreenHeight() --Get the Vertical resolution of the setup
-		fixX = GetScreenWidth() --Get the Horizontal resolution of the setup
-		fixYdiv = fixY/9
-
-		if fixX > (fixYdiv*21) then --If it's bigger than a 21:9 monitor (so multiple monitors)
-
-			if GetScreenWidth() >= (fixYdiv*53) then --Figure out if at least one display is Ultrawide
-				fixX = (fixYdiv*21) --Calculate the Horizontal resolution of the middle display for 21:9 Aspect Ratio
-				aspect = "21:9"
-			elseif GetScreenWidth() == (fixYdiv*36) then --Figure out if all 3 displays are 4:3
-				fixX = (fixYdiv*12) --Calculate the Horizontal resolution of the middle display for 4:3 Aspect Ratio
-				aspect = "4:3"
-			else
-				fixX = (fixYdiv*16) --Calculate the Horizontal resolution of the middle display for 16:9 Aspect Ratio
-				aspect = "16:9"
-			end
-		end
-	end
-
-	function sfixAnnounce() --Chatspam function
-		print("~Surround Fix~")
-
-		if GetScreenWidth() <= (fixYdiv*21) then --If it's smaller than or equal to a 21:9 monitor (so single monitor), Print this
-			print("Single display detected")
+	if fixX > (fixYdiv*21) then --If it's bigger than a 21:9 monitor (so multiple monitors)
+		if GetScreenWidth() >= (fixYdiv*53) then --Figure out if at least one display is Ultrawide
+			fixX = (fixYdiv*21) --Calculate the Horizontal resolution of the middle display for 21:9 Aspect Ratio
+			aspect = "21:9"
+		elseif GetScreenWidth() == (fixYdiv*36) then --Figure out if all 3 displays are 4:3
+			fixX = (fixYdiv*12) --Calculate the Horizontal resolution of the middle display for 4:3 Aspect Ratio
+			aspect = "4:3"
 		else
-			print("Middle display detected as", aspect)
+			fixX = (fixYdiv*16) --Calculate the Horizontal resolution of the middle display for 16:9 Aspect Ratio
+			aspect = "16:9"
 		end
 	end
+end
 
 
-	function UIParentHook(self) --self is needed so it gets passed in on the hook
-		if coreSet then --Makes it so that if coreSet is true, it doesn't run the changes.
-			return
-		end
+local function sfixAnnounce() --Chatspam function
+	print("~Surround Fix~")
 
-		coreSet = true --Sets coreSet to true so it doesn't trigger from itsself
+	if GetScreenWidth() <= (fixYdiv*21) then --If it's smaller than or equal to a 21:9 monitor (so single monitor), Print this
+		print("Single display detected")
+	else
+		print("Middle display detected as", aspect)
+	end
 
-			print("Hook Fired")
-			uiResolution()
-			print("Setting UI Resolution post res", floor(fixX+0.5),"x",floor(fixY+0.5))
-			sfixAnnounce()
-			print("Setting UI Resolution post announce", floor(fixX+0.5),"x",floor(fixY+0.5))
+	if parentDefault then --If he haven't touched the default UIParent behaviour, use a different message.
+		print("Leaving UIParent as default")
+	else
+		print("Setting UI Resolution to", floor(fixX+0.5),"x",floor(fixY+0.5))
+	end
+end
 
-			self:SetSize(fixX, fixY) --self is UIParent since that's what the hook is
-			self:ClearAllPoints()
-			self:SetPoint("CENTER")
 
+local function UIParentHook(self) --self is needed so it gets passed in on the hook
+	if coreSet then --Makes it so that if coreSet is true, it doesn't run the changes.
+		return
+	end
+
+	coreSet = true --Sets coreSet to true so it doesn't trigger from itsself
+	uiResolution()
+
+	if GetScreenWidth() <= (fixYdiv*21) and parentDefault then --If it's smaller than or equal to a 21:9 monitor (so single monitor), do nothing until it's been changed.
 		coreSet = nil
+		sfixAnnounce()
+		return
 	end
+
+	parentDefault = nil --Set this to nil forever, since there's no longer the default UIParent behaviour
+	sfixAnnounce()
+	self:SetSize(fixX, fixY) --self is UIParent since that's what the hook is
+	self:ClearAllPoints()
+	self:SetPoint("CENTER")
+	coreSet = nil
+
+end
+
+
+
+--------------------------------------------------------------------------------
+--Event Registration
+--------------------------------------------------------------------------------
+sfixFrame:RegisterEvent("ADDON_LOADED")
+sfixFrame:RegisterEvent("DISPLAY_SIZE_CHANGED")
+
+
+
+--------------------------------------------------------------------------------
+--Event Handler
+--------------------------------------------------------------------------------
+sfixFrame:SetScript("OnEvent", function(self, event, ...) --This is essentially saying "On an event, run this function of everything below"
 
 	if event == "ADDON_LOADED" then
 		sfixFrame:UnregisterEvent("ADDON_LOADED")
-		print("Addon Loaded")
-		local coreSet
-		local coreSet2
 		hooksecurefunc(UIParent, "SetPoint", UIParentHook)--Hooks into UIParent "SetPoint", so if anything tries to change that then it runs
---		hooksecurefunc(UIParent, "SetScale", UIParentHook) --Hooks into UIParent "SetScale", so if anything tries to change that then it runs
-
+		hooksecurefunc(UIParent, "SetScale", UIParentHook) --Hooks into UIParent "SetScale", so if anything tries to change that then it runs
 	end
 
-    if event == "PLAYER_LOGIN" or event == "DISPLAY_SIZE_CHANGED" then --Main part of the code that runs when the events happen
-		eventUnregister() --Unregister the events so it doesn't spam
-		C_Timer.After(0.1, function() end) --Run the main code after 0.1 seconds to prevent issues
-
-		C_Timer.After(rateLimit, function() eventRegister() end) --After the rateLimit amount of time, reregister the events, run the main code again, and print to the chat box
+    if event == "DISPLAY_SIZE_CHANGED" then --Main part of the code that runs when the events happen
+		sfixFrame:UnregisterEvent("DISPLAY_SIZE_CHANGED") --Unregister the events so it doesn't spam
+		C_Timer.After(rateLimit, function() UIParent:SetPoint("CENTER") sfixFrame:RegisterEvent("DISPLAY_SIZE_CHANGED") end) --After the rateLimit amount of time, reregister the events, run the main code again, and print to the chat box
 
     end
+
 end)
-
-
-
-
-
-
-
-
---		C_Timer.After(2, function() eventRegister() print("timer1") end) - This style of having a function and then two things with an end at the end lets you invoke multiple functions since you're making a mini function in there
---floor(x+0.5) rounds a number normally.
