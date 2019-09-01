f03d78a605ecb076aed9fd31016e604ed63b9102
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-01 04:09:37 +0100

    Change to frame based function that runs on event

--- SurroundFix.lua
+++ SurroundFix.lua
@@ -1,9 +1,17 @@
-SurroundFix = {}
+local sfixFrame = CreateFrame("Frame", "SurroundFixFrame")
 
-function SurroundFix:runFix()
-    UIParent:SetSize(1920, 1080)
-	UIParent:ClearAllPoints()
-	UIParent:SetPoint("CENTER")
-end
+    sfixFrame:RegisterEvent("PLAYER_LOGIN")
+    sfixFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+    sfixFrame:RegisterEvent("ADDON_LOADED")
+	sfixFrame:RegisterEvent("PLAYER_ALIVE")
+	sfixFrame:RegisterEvent("SPELLS_CHANGED")
 
-SurroundFix:runFix()
+
+    sfixFrame:HookScript("OnEvent", function(self, event, ...)
+        if event == "PLAYER_LOGIN" or "ADDON_LOADED" or "PLAYER_ENTERING_WORLD" or "PLAYER_ALIVE" or "SPELLS_CHANGED" then
+			UIParent:SetSize(1920, 1080)
+            UIParent:ClearAllPoints()
+            UIParent:SetPoint("CENTER")
+        end
+
+    end)
